eeaba58cb6c5f8b1e461ed723efc2c44d3ddb6fb
Keith T. Star <keith@uberfoo.com>
2022-11-11 17:08:33 -0700

    Cleaned up. Got rid of warnings/errors/misuse.

--- src/features/paper/Paper.tsx
+++ src/features/paper/Paper.tsx
@@ -8,11 +8,10 @@ import { selectObjects } from '../object/objectSlice';
 import { selectPaperById, selectPapers, getPaperIds } from './paperSlice';
 
 import styles from './Paper.module.css';
-// import { selectAll } from '@testing-library/user-event/dist/types/event';
 
-const width = 3200;
-const height = 1600;
-const gridSize = 25;
+const defaultWidth = 3200;
+const defaultHeight = 1600;
+const defaultGridSize = 25;
 const defaultScale = 1.25;
 const minScale = 0.4;
 const maxScale = 4.5;
@@ -24,72 +23,15 @@ interface PaperProps {
 }
 
 export function Paper(props: PaperProps) {
-    // All the bits we need to draw ourselves
-    // x: number;
-    // y: number;
-    // scale: number;
-    // ref: SVGGElement | null;
-    // x_lines: Array<JSX.Element>;
-    // y_lines: Array<JSX.Element>;
-
-    // state: PaperStore;
-    // //
-    // // Things needed to pan and zoom
-    // mouseDown: Boolean;
-    // // gestureHandler;
-
-    // constructor(props: PaperProps) {
-
-    //     super(props);
-    //     let [x, y] = defaultPosition;
-    //     this.x = x;
-    //     this.y = y;
-    //     this.scale = defaultScale;
-    //     this.ref = null;
-    let x_lines = [];
-    for (let i = 0; i < height + 1; i += gridSize) {
-        x_lines.push(<line x1={0} y1={i} x2={width} y2={i} />);
-    }
-
-    let y_lines = [];
-    for (let i = 0; i < width + 1; i += gridSize) {
-        y_lines.push(<line x1={i} y1={0} x2={i} y2={height} />);
-    }
-
-    //     this.state = {
-    //         width: width,
-    //         height: height,
-    //         domain_name: props.domain,
-    //         domain_ns: props.domain_ns,
-    //         // objects: [],
-    //         // attributes: [],
-    //         // relationships: []
-    //     };
-
-    //     console.log(this.state);
-
-    //     this.mouseDown = false;
-    //     // this.gestuerHandler = useDrag(())
-
-    //     // Bind Callbacks to this. Still?
-    //     this.onMouseDownHandler = this.onMouseDownHandler.bind(this);
-    //     this.onMouseUpHandler = this.onMouseUpHandler.bind(this);
-    //     this.onMouseMoveHandler = this.onMouseMoveHandler.bind(this);
-    // }
-
-    // componentDidMount(): void {
-    // this.ref!.onmouseDown = this.onMouseDownHandler;
-    // }
-
     let [move, setMove] = useState({ mouseDown: false, x: defaultPosition[0], y: defaultPosition[1] });
 
     let onMouseDownHandler = (event: React.MouseEvent) => {
-        // console.log(`down: ${this}`);
+        // This forces an update -- bad here.
         setMove({ ...move, mouseDown: true });
-        // styles.paperBase.cursor = "grabbing";
     }
 
     let onMouseUpHandler = (event: React.MouseEvent) => {
+        // This forces an update -- bad here.
         setMove({ ...move, mouseDown: false });
     }
 
@@ -100,26 +42,36 @@ export function Paper(props: PaperProps) {
             x += event.movementX;
             y += event.movementY;
 
+            // This forces an update -- good here.
             setMove({ mouseDown, x, y });
-
-            // forceUpdate();
         }
     }
 
-    // render(): React.ReactNode {
-    let paperIds: Array<string> = useAppSelector((state) => getPaperIds);
+    let paperIds: Array<string> = useAppSelector((state) => getPaperIds(state));
     let paper: PaperStore | undefined = useAppSelector((state) => selectPaperById(state, paperIds[0]));
+
+
     let objects: Array<ObjectStore> = useAppSelector((state) => selectObjects(state));
     let objectInstances: Array<JSX.Element> = objects.map((o) => {
         return <Object key={o.id} id={o.id} />
     });
+
+    let x_lines = [];
+    for (let i = 0; i < paper!.height + 1; i += defaultGridSize) {
+        x_lines.push(<line x1={0} y1={i} x2={paper!.width} y2={i} />);
+    }
+
+    let y_lines = [];
+    for (let i = 0; i < paper!.width + 1; i += defaultGridSize) {
+        y_lines.push(<line x1={i} y1={0} x2={i} y2={paper!.height} />);
+    }
 
     let { mouseDown, x, y } = move;
 
     return (
         <g id="paper" pointerEvents="all" transform={"translate(" + x + "," + y + ") scale(" + defaultScale + ")"}
             onMouseDown={onMouseDownHandler} onMouseUp={onMouseUpHandler} onMouseMove={onMouseMoveHandler} onMouseLeave={onMouseUpHandler}>
-            < rect id="background" width={width} height={height} className={styles.paperBase} />
+            < rect id="background" width={paper!.width} height={paper!.height} className={styles.paperBase} />
             <g className={styles.axis}>
                 {x_lines}
             </g>
@@ -131,25 +83,4 @@ export function Paper(props: PaperProps) {
             </g>
         </g >
     )
-    // }
 }
-//     function onMouseDownHandler(event: React.MouseEvent) {
-//         // console.log(`down: ${this}`);
-//         this.mouseDown = true;
-//     }
-
-//     function onMouseUpHandler(event: React.MouseEvent) {
-//         // console.log(`up: ${this}`);
-//         this.mouseDown = false;
-//     }
-
-//     function onMouseMoveHandler(event: React.MouseEvent) {
-//         // If mouseDown we are panning. This is wrong, and actually needs to start drawing.
-//         if (this.mouseDown) {
-//             this.x += event.movementX;
-//             this.y += event.movementY;
-
-//             this.forceUpdate();
-//         }
-//     }
-// }
